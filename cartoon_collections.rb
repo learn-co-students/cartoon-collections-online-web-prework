b4fcaff1c189d11060b465e1e64a3f35e17d0c3d
Brandon Teich <brandonteich123@gmail.com>
2018-10-04 21:27:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,22 +10,6 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls(array)
- if array.length >= 4 
-   true
- else
-   false
- end
-end
-
-def find_the_cheese(array)
-  array = def roll_call_dwarves(array)
-  array.each_with_index do 
-    |dwarf, i|
-    puts "#{i + 1} #{dwarf}"
-  end
-end
-
 def summon_captain_planet(array)
   array.collect do |i|
     i.capitalize + "!"
@@ -41,24 +25,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array = ["cheddar", "gouda", "camembert"]
-  array.each do |cheese|
-    if cheese.include?("cheddar" || "gouda" || "camembert") == true
-      cheese
-    elsif cheese.include?("cheddar" || "gouda" || "camembert") == false
-      nil
-      end
-    end
-  nil
+  cheeses = ["cheddar", "gouda", "camembert"]
+  array.find do |array| 
+    cheeses.include?(array)
+  end
 end
-
-  array.each do |cheese|
-    if cheese.include?("cheddar" || "gouda" || "camembert") == true
-      cheese
-    elsif cheese.include?("cheddar" || "gouda" || "camembert") == false
-      nil
-      end
-    end
-end
-
 
