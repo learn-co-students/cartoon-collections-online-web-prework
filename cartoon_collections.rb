842e7e8f4324717ea699f5a67f6c95f625f1e7cc
Brandon Teich <brandonteich123@gmail.com>
2018-10-04 21:27:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,64 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, i|
+    puts "#{i + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |i|
+    i.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+ if array.length >= 4 
+   true
+ else
+   false
+ end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array = def roll_call_dwarves(array)
+  array.each_with_index do 
+    |dwarf, i|
+    puts "#{i + 1} #{dwarf}"
+  end
 end
+
+def summon_captain_planet(array)
+  array.collect do |i|
+    i.capitalize + "!"
+  end
+end
+
+def long_planeteer_calls(array)
+ if array.length >= 4 
+   true
+ else
+   false
+ end
+end
+
+def find_the_cheese(array)
+  array = ["cheddar", "gouda", "camembert"]
+  array.each do |cheese|
+    if cheese.include?("cheddar" || "gouda" || "camembert") == true
+      cheese
+    elsif cheese.include?("cheddar" || "gouda" || "camembert") == false
+      nil
+      end
+    end
+  nil
+end
+
+  array.each do |cheese|
+    if cheese.include?("cheddar" || "gouda" || "camembert") == true
+      cheese
+    elsif cheese.include?("cheddar" || "gouda" || "camembert") == false
+      nil
+      end
+    end
+end
+
+
