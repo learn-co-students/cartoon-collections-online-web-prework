01207283e6ba4e42cedd4e9906b7b5b1f3504858
beaulieudev <beaulieudev@gmail.com>
2018-10-18 00:38:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index do |a, i|
+    puts "#{a}"
 end
 
 def summon_captain_planet# code an argument here
