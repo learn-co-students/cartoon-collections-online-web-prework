c9017c4fb743036bab8bb267cbb4071668cb0458
A Glazer <avigayilglazer@gmail.com>
2019-01-03 20:07:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,12 @@ end
 def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if food.include?(cheese_types) == true
-    return food.find(cheese_types) {|word| return word}
+  if food.include?("cheddar") == true
+    return "cheddar"
+  elsif food.include?("gouda") == true
+    return "gouda"
+  elsif food.include?("camembert") == true
+    return "cammembert"
   else
     return nil
   end
