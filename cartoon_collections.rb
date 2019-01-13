afb7c2040dfb5b431662d83f76c427ffd72421ce
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-13 01:56:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  dwarves.each.with_index (1) do |name, index|
+    puts "#{index} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map do |element|
+    element.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  x = array.select do |y|    #returns an array with only strings with more than 4 characters
+    y.length > 4
+  end
+  
+  if x.size > 0             # if the array is empty then all string are shorter than 4
+    return true 
+  else 
+    return false 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  food.find do |type|                  #find will return first element that is true
+    cheese_types.include? (type)       #array of cheese_types is compared against each element
+  end                                  #of food array. The first time include returns true will
+end                                    #return that element via find method.
