f8f8b72aff06d1677aa696bada922195a5a7564e
Eyal Toledano <eyaltoledano@me.com>
2019-01-16 00:55:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,11 +16,16 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.each_with_index do |item|
+    return item if item == cheese_types.any?
+  end
 end
