beb526ebf5c067f43c0546950a3939b29f3ed996
Kelly Sciandra <kellysciandra@icloud.com>
2019-02-21 16:02:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,29 +1,25 @@
 def roll_call_dwarves(dwarves)
-name = dwarves.join(" ")
-counter = 0 
-  line = ""
-  dwarves.each_with_index { |name,index| line << "#{index+1}. #{name}"}
+counter = 1
+message = []
+  dwarves.each do |x|
+  puts "#{counter}. #{x} "
 counter += 1
-  puts line
+  end
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |x|
-  x.upcase[0] + x[1..20] + "!"
-  end
+planeteer_calls.collect do |x|
+x.upcase[0] + x[1..20] + "!"
+end
 end
 
 def long_planeteer_calls(calls)
-  calls.any? do |calls|
-    calls.length > 4
-  end
+  calls.length > 4
 end
 
 
 def find_the_cheese(cheese)
-cheese_types = ["cheddar", "gouda", "camembert"]
-variable = cheese_types.join
-  if cheese.include?("cheddar")
+if cheese.include?("cheddar")
     return "cheddar"
   else
     return nil
