8abac748f7888d6aba140e756fe9a69ecc4201dd
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-24 06:34:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,4 @@
-dwarves = [
-  Doc
-  Dopey
-  Bashful
-  Grumpy
-  Sneezy
-  Sleepy
-  Happy
-  ]
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy"]
 
 def roll_call_dwarves (dwarves)
   dwarves.each_with_index do |dwarf, index|
