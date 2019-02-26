319a183ca4d7d63b1bce16d1e78ef0dc8b96f06b
Dennis Chung <chungdennis6@gmail.com>
2019-02-26 00:52:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  planeteer_calls.collect do |planeteer_call|
+    planeteer_call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  planeteer_calls.any? do |planeteer_call|
+    planeteer_call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  cheese_types.each do |cheese_type|
+    if ingredients.include?(cheese_type)
+      return cheese_type
+    else
+      return nil
+    end
+  end
