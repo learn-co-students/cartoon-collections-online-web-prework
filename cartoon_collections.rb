37c7bd972456f312a2329e899a3a2b41263390de
Samuel W Tong <ssamt1142@gmail.com>
2019-02-26 02:49:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(array)
+  array.each_with_index do |n, r|
+    puts "#{r+1}. #{n}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |x|
+    x.capitalize! << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arg)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arg.find do |x|
+    cheese_types.include?(x) == true
+  end
 end
