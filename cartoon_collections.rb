888e4ef16c8a6d52984d85b8f6e4ba76d8c7e3ae
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 18:35:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index {|element, index| puts "#{index+1}. #{element}" }
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  newarray = []
+  array.each do |element|
+    newarray.push(element.capitalize << "!")
+    newarray.join(" ")
+  end
+  return newarray
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|item| cheese_types.include?(item)}
 end
