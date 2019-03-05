540d93ffc8b3aae8ebbe8a73be4f1bcef1ab5c01
David Thayer <dthayer16@gmail.com>
2019-03-05 14:01:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(calls)
-  calls.collect {|word| word.reverse}.join(" ")
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |call| call.capitalize + '!' }
 end
 
 def long_planeteer_calls(words)
@@ -21,4 +21,11 @@ def find_the_cheese(array)
   if array include?(cheese_types)
   else
   end
-end}
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
+end
