f85e532221f590c8e31ea383adf84bc2701cb54f
Angela <malaika.mumba@gmail.com>
2019-05-08 23:13:43 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,15 @@
 def roll_call_dwarves(dwarfs)
   dwarfs.each_with_index do|list,index|
-    
+
     puts "#{index+1}. #{list}"
   end
-end 
+end
 
 # def summon_captain_planet(the_planet)
-  
+
 #   the_planet.map { |power| power.capitalize + "!"}
-  
-#   end 
+
+#   end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + "!" }
@@ -18,7 +18,7 @@ end
 def long_planeteer_calls(calls_long)
   calls_long.any? do |calls_long|
     calls_long.length > 4
-  end 
+  end
 end
 
 def find_the_cheese(cheesy_items)
@@ -27,14 +27,3 @@ def find_the_cheese(cheesy_items)
     cheeses.include?(maybe_cheese)
   end
 end
-
-
-# def reverse_each_word(sentence)
-#   sentence.split(" ").collect {|word| word.reverse}.join(" ")
-# end
-
-# def reverse_each_word(sentence)
-#   arr =[]
-#   sentence.split.each {|word| arr << word.reverse}
-#   arr.join(" ")
-# end
