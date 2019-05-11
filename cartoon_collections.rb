a0de633d568c539ae217f4514967bbff1f9f4548
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 07:10:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def Roll_call_dwarves = Roll_call_dwarves.new 
+  
+  ["Doc","Dopey","Bashful","Grumpy"].each_with_index { |item,index|
+  
+  Roll_call_dwarves [item] = index 
+  
+  }
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def Summon_captain_planet = ["puff","go","two"]
+  Summon_captain_planet.map {|name| name.capitalize }
 end
 
 def long_planeteer_calls# code an argument here
