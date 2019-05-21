32a68f560d6869fd098a5f5b52f2b5a951ab2847
Jack Atherton <jackrrye@gmail.com>
2019-05-21 19:30:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry" 
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index| 
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |x| 
+    x.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  words_over_4 = 0
+  array.each do |x|
+    if x.size > 4
+      words_over_4+=1
+    else
+      words_over_4
+    end
+  end
+  if words_over_4 > 0 
+    return true 
+  else
+    return false 
+  end
 end
 
 def find_the_cheese# code an argument here
