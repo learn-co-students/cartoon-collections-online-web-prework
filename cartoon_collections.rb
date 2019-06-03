3cfdcc179169fab60d32da25fcfdd44edc1c1310
kpobrien4 <48386395+kpobrien4@users.noreply.github.com>
2019-06-02 20:43:11 -0400

    Update cartoon_collections.rb
    Created the required methods to complete the tasks using enumerator methods.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  array.collect do |word|
+     word.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  short_words = ["puff", "go", "two"]
+ assorted_words = ["two", "go", "industrious", "bop"]
+  array.any? do |word|
+     word.length > 4
+   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+ingredients = ["garlic", "bread", "rosemary"]
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end
+  end
 end
