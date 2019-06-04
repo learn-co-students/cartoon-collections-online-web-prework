857d07987d0318cc5c71189d7f10c60c789794f1
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-04 11:42:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,20 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? do |word| 
-    word.length >= 4
+  if array.any?{|word| word.length > 4}
+    return true
+  else array.any?{|word| word.length < 4}
+    return false
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  if cheese_types.find do |cheese|
+  array.include?(cheese)
+    return "#{cheese_types}"
+  else
+    return nil
+  end
 end
